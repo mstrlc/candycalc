45727659b66363a18619699158f415d945c6aea4
Matyáš Strelec <matyasstrelec@gmail.com>
2022-04-16 11:33:51 +0200

    edit GUI so invalid button sequences can't be entered

--- src/main.py
+++ src/main.py
@@ -9,44 +9,71 @@ class mainWindow(QMainWindow, Ui_mainWindow):
         self.setupUi(self)
         self.show()
 
-        self.pushButton0.clicked.connect(self.addCharacter)
-        self.pushButton1.clicked.connect(self.addCharacter)
-        self.pushButton2.clicked.connect(self.addCharacter)
-        self.pushButton3.clicked.connect(self.addCharacter)
-        self.pushButton4.clicked.connect(self.addCharacter)
-        self.pushButton5.clicked.connect(self.addCharacter)
-        self.pushButton6.clicked.connect(self.addCharacter)
-        self.pushButton7.clicked.connect(self.addCharacter)
-        self.pushButton8.clicked.connect(self.addCharacter)
-        self.pushButton9.clicked.connect(self.addCharacter)
-        self.pushButtonDec.clicked.connect(self.addCharacter)
-        self.pushButtonLBrac.clicked.connect(self.addCharacter)
-        self.pushButtonRBrac.clicked.connect(self.addCharacter)
-        self.pushButtonLn.clicked.connect(self.addCharacter)
-        self.pushButtonFact.clicked.connect(self.addCharacter)
-        self.pushButtonRoot.clicked.connect(self.addCharacter)
-        self.pushButtonExp.clicked.connect(self.addCharacter)
-        self.pushButtonDiv.clicked.connect(self.addCharacter)
-        self.pushButtonMul.clicked.connect(self.addCharacter)
-        self.pushButtonMin.clicked.connect(self.addCharacter)
-        self.pushButtonPlus.clicked.connect(self.addCharacter)
+        # Connect buttons to functions
+
+        # Digit buttons
+        self.pushButton0.clicked.connect(self.addDigit)
+        self.pushButton1.clicked.connect(self.addDigit)
+        self.pushButton2.clicked.connect(self.addDigit)
+        self.pushButton3.clicked.connect(self.addDigit)
+        self.pushButton4.clicked.connect(self.addDigit)
+        self.pushButton5.clicked.connect(self.addDigit)
+        self.pushButton6.clicked.connect(self.addDigit)
+        self.pushButton7.clicked.connect(self.addDigit)
+        self.pushButton8.clicked.connect(self.addDigit)
+        self.pushButton9.clicked.connect(self.addDigit)
+
+        # Decimal point button
+        self.pushButtonDec.clicked.connect(self.addDecimal)
+
+        # Bracket buttons
+        self.pushButtonLBrac.clicked.connect(self.addBracket)
+        self.pushButtonRBrac.clicked.connect(self.addBracket)
+
+        # Function buttons
+        self.pushButtonLn.clicked.connect(self.addFunction)
+        self.pushButtonFact.clicked.connect(self.addFunction)
+        self.pushButtonRoot.clicked.connect(self.addFunction)
+        self.pushButtonExp.clicked.connect(self.addFunction)
+        self.pushButtonDiv.clicked.connect(self.addFunction)
+        self.pushButtonMul.clicked.connect(self.addFunction)
+        self.pushButtonMin.clicked.connect(self.addFunction)
+        self.pushButtonPlus.clicked.connect(self.addFunction)
+
+        # Other buttons
         self.pushButtonDel.clicked.connect(self.deleteDigit)
         self.pushButtonCE.clicked.connect(self.clearAll)
         self.pushButtonEq.clicked.connect(self.finishCalculation)
 
-    def addCharacter(self):
+    def addDigit(self): # Add digit to the display
         self.labelMain.setText(self.labelMain.text() + self.sender().text())
 
-    def deleteDigit(self):
+    def addDecimal(self): # Add decimal point to the display, only if the last character is a digit
+        if(self.labelMain.text()[-1].isdigit()):
+            self.labelMain.setText(self.labelMain.text() + ".")
+
+    def addBracket(self): # Add a bracket to the display
+        if(self.sender().text() == "("): # Add opening bracket only if the the previous bracket was closed
+            if(self.labelMain.text().count("(") == self.labelMain.text().count(")")):
+                self.labelMain.setText(self.labelMain.text() + self.sender().text())
+        elif(self.sender().text() == ")"): # Add closing bracket if the previous bracket is open, not closed
+            if(self.labelMain.text().count("(") == self.labelMain.text().count(")") + 1):
+                self.labelMain.setText(self.labelMain.text() + self.sender().text())
+
+    def addFunction(self): # Add a function to the display
+        if(self.labelMain.text()[-1].isdigit() or self.labelMain.text()[-1] == ")"): # If previous character is a digit or a closing bracket
+            self.labelMain.setText(self.labelMain.text() + self.sender().text())
+
+    def deleteDigit(self): # Delete the last character
         self.labelMain.setText(self.labelMain.text()[:-1])
 
-    def clearAll(self):
+    def clearAll(self): # Clear everything
         self.labelSecond.setText("")
         self.labelMain.setText("")
 
-    def finishCalculation(self):
-        self.labelSecond.setText(self.labelMain.text())
-        self.labelMain.setText(str(eval(self.labelMain.text())))
+    def finishCalculation(self): # Calculate the expression
+        self.labelSecond.setText(self.labelMain.text()) # Copy the expression to the second display
+        self.labelMain.setText(str(eval(self.labelMain.text()))) # Calculate the expression and display the result TODO send to our own math parse function
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
