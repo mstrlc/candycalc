63f6bc4b8f7b84a0f867aaf4e57cdcfcbed5790d
Seidly <seidlondra@gmail.com>
2022-04-25 17:04:52 +0200

    adding operators, coding basic input parsing

--- src/parsefunc.py
+++ src/parsefunc.py
@@ -1,4 +1,23 @@
-def solve_parentheses(input):
-    for c in input:
-        if(0)
-        
+# -*- coding: utf-8 -*-
+
+operators = ["ln", "!", "√", "^", "×", "/", "+", "-"]
+precedence = [["ln"], ["!"], ["√" ,"^"], ["×", "/"], ["+", "-"]]
+
+def check_num(str):
+    try:
+        print(str)
+    except:
+        exit
+
+def parse_input(input_str):
+    for set in precedence:
+        for index ,c in enumerate(input_str):
+            if c == '(':
+                solve_expr(input_str[index:input_str.find(')')])
+
+
+def solve_expr(expr):
+    exit
+
+
+
