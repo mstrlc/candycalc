2f1939b903551781780b1739d958ed832a232724
Maxmilián Nový <101198134+Volvundur@users.noreply.github.com>
2022-04-27 12:45:10 +0200

    fixed nroot function, changed error messages
    fixed nroot function not properly working for negative numbers

--- src/mathlib.py
+++ src/mathlib.py
@@ -1,6 +1,5 @@
 from math import log
 
-
 def add(a,b):
     if type(a) == str or type(b) == str:
         raise TypeError
@@ -28,27 +27,30 @@ def factorial(a):
     if type(a) == str:
         raise TypeError
     if a<0:
-        raise ValueError("Factorial menej ako 0")
+        raise ValueError("Math Err")
     b=1
     for i in range(a):
         b*=(i+1)
     return b
 
-def power(a,b):
+def power(a,b): 
     if type(b) != int or b<0:
-        raise ValueError("Exponent nie je prirodzene cislo")
+        raise ValueError("Math Err")
     if type(a) == str or type(b) == str:
         raise TypeError
     return a**b
 
-def sqrt(a,b):
+def nroot(a,b): 
     if type(a) == str or type(b) == str:
         raise TypeError
     if b<0:
-        raise ValueError("Zaporna odmocnina")
+        raise ValueError("Math Err")
     if a<0 and b%2==0:
-        raise ValueError("Negativne cislo v parnej odmocnine")
-    return a**(1/b)
+        raise ValueError("Math Err")
+    elif a<0:
+        return -((-a)**(1/b))
+    else:
+        return a**(1/b)
 
 def ln(a):
     if type(a) == str:
