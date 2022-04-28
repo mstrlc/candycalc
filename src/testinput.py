5b9c57cad1703102de663d708cf729590d7f69cb
Seidly <seidlondra@gmail.com>
2022-04-28 19:36:12 +0200

    added get_parsed_input function to testinput

--- src/testinput.py
+++ src/testinput.py
@@ -4,6 +4,6 @@ import sys
 for line in sys.stdin:
     #prints parsed input as a list
     print(parsefunc.parse_input(line.rstrip()))
-
+    print (parsefunc.get_parsed_input(line.rstrip()))
     #prints the result after solving the expression
     print(parsefunc.get_result(line.rstrip()))
