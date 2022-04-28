ab303f3796f6f3fd6e85bca4c6e138773724aafc
Seidly <seidlondra@gmail.com>
2022-04-28 22:32:42 +0200

    added doxy comments, cleaned up any debug printing

--- src/parsefunc.py
+++ src/parsefunc.py
@@ -2,52 +2,65 @@
 import mathlib
 import sys
 
-#sys.tracebacklimit=0
-parentheses = ["(", ")"] #supported parentheses
-operators = ["ln", "!", "√", "^", "×", "/", "+", "-"] #supported operators
-precedence = [["(",")"],["ln"], ["!"], ["√" ,"^"], ["×", "/"], ["+", "-"]] #set precendence of operatos
-
-##
-# @package parsefunc
-# parsefunc is a function input parsing function for Candycalc
+## @package parsefunc
+# @brieef parsefunc is a module that handles input parsing for Candycalc
 #
 # To use this module either use:
 # parse_input(input) for parsing input to expression
 # solve_expr(expr) to solve the expression
 # get_result(input) to get a result of 2 functions above
+# get_parsed_input(input) to get string representing the expression
+
+parentheses = ["(", ")"] #supported parentheses
+operators = ["ln", "!", "√", "^", "×", "/", "+", "-"] #supported operators
+precedence = [["(",")"],["ln"], ["!"], ["√" ,"^"], ["×", "/"], ["+", "-"]] #set precendence of operatos
+
+
+##
+# @brief Solves expression in given list
 #
-
-#TODO: after merge adjust the funcion for mathlib operations
-
+# @param expr Python [string] list
+# @return Result of expression
 def solve_expr(expr):
+    # nothing left to solve, return result
     if len(expr) == 1:
         return expr[0]
+    # nothing in the expression
     if len(expr) == 0:
         return 0
-
+        
+    # loop for each set of preceeding operators 
     for op_set in precedence:
         index = 0
-        
+
+        # loop for the whole list
         while len(expr) > index:
             token = expr[index]
             if token in op_set:
 
+                # parentheses handling
                 if token in '(':
-                    
+
                     help = []
+                    # first item in parentheses is operator
                     if expr[index+1] in "+-":
                         help.append(0)
-
+                    # getting the expression in parentheses
                     help += expr[index+1:expr.index(')')]
-                    print(help)
+
+                    # checking if there is anything to solve
                     if len(help) == 0:
                         return 0
                     if index != 0 and expr[index-1]=="√":
                         index+=1
+
+                    # if there is call recursion
+                    # remove the "(string)" from the list
                     elif len(help) != 0:
                         expr[index] = solve_expr(help)
                         del expr[index+1:expr.index(')')+1]
 
+                # operators handling 
                 if token in operators:
 
                     if token == "ln":
@@ -60,7 +73,7 @@ def solve_expr(expr):
                         print(expr)
 
                     elif token == "√":
-                        print("haha")
+                        # checking wheter the n of root is present in the list
                         if ',' in expr[index+1:expr.index(')')]:
                             print(expr[index+2])
                             expr[index] = mathlib.nroot(float(expr[index+2]), int(expr[index+4]))
@@ -95,19 +108,15 @@ def solve_expr(expr):
 
             index+=1
     return solve_expr(expr)
+#end of solve_expr func 
 
+
+##
+# @brief Parses string input into expression in [str] list
+#
+# @param input Any string
+# @return Python [string] list
 def parse_input(input):
-    '''
-    Parses input into string list
-
-    args:
-    ----------  
-    string : input
-        input string
-
-    returns [string]
-        parsed 
-    '''
 
     str = input     # input string
     parsed = []     # already parsed string
@@ -207,6 +216,7 @@ def parse_input(input):
             if parsed[len(parsed)-1] != '!':
                 raise SyntaxError()
 
+    # complete unclosed braces
     open = 0
     close = 0            
     for i, check in enumerate(parsed):
@@ -214,6 +224,7 @@ def parse_input(input):
             open+=1
         elif check == ')':
             close+=1
+        # check for "!n!n" -> "!n×!n"
         elif check == '!':
             if i!=len(parsed)-1:
                 if parsed[i-1].isnumeric() and parsed[i+1].isnumeric():
@@ -224,32 +235,53 @@ def parse_input(input):
 #end of parse_input func
 
 
+##
+# @brief Parses input into more readable mathematical string
+#
+# @param input - Any input string
+# @return String representing the mathematical expression 
 def get_parsed_input(input):
+    # get the parsed list
     parsed=parse_input(input)
     string=""
+
+    # check for weird number strings eg. "002" -> "2"
     for i, item in enumerate(parsed):
         if item.isnumeric() and item != '0':
             parsed.insert(i,round(int(item)))
             del parsed[i+1]
+
+        # "()" -> "(0)"
         elif item == '(':
             if parsed[i+1] == ')':
                 print(parsed[i+1])
                 parsed.insert(i,int('0'))
                 break
+
+    # make string out of the final list
     for item in parsed:
         string+=str(item)
     return string
+#end of get_parsed_input func
 
-#simple getr_result func 
+
+##
+# @brief Calls the needed functions for a result
+#
+# @param input - Any input string
+# @return Either a float or int depending on the result 
 def get_result(input):
-    #call of input/expr parsing func    
+    # call of input/expr parsing func    
     result = float(solve_expr(parse_input(input)))
 
+    # check lenght of the number
     lenght = len(str(int(result)))
     if lenght > 11:
         raise OverflowError()
 
+    # decide on it's type and representation
     if result % 1 == 0:
         return int(result)
     else:
         return round(result, 12-lenght)
+#end of get_result func
